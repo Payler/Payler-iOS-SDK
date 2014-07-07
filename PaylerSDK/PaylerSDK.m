d9468616e8dac4824373fcfe3cba25b26cbf9c15
Maxim Pavlov <mp@poloniumarts.com>
2014-07-07 12:53:04 +0400

    Initial commit

---
+++ PaylerSDK/PaylerSDK.m
@@ -0,0 +1,13 @@
+//
+//  PaylerSDK.m
+//  PaylerSDK
+//
+//  Created by Максим Павлов on 07.07.14.
+//  Copyright (c) 2014 Polonium Arts. All rights reserved.
+//
+
+#import "PaylerSDK.h"
+
+@implementation PaylerSDK
+
+@end
