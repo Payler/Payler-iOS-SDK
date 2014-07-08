5a955b49299df74bfa910ee31337d848a93d87e6
Maxim Pavlov <mp@poloniumarts.com>
2014-07-08 10:29:46 +0400

    [add][SDK-23] - Запросы StartSession, Pay

--- PaylerSDK/PLRWebView.m
+++ PaylerSDK/PLRWebView.m
@@ -9,14 +9,35 @@
 #import "PLRWebView.h"
 #import "PLRSessionInfo.h"
 #import "PLRPayment.h"
+#import <AFHTTPRequestOperationManager.h>
 
 static NSString *const APIBaseURL = @"https://sandbox.payler.com/gapi";
 
 @interface PLRWebView ()<UIWebViewDelegate>
 
+@property (nonatomic, copy) PLRCompletionBlock completionBlock;
+
 @end
 
 @implementation PLRWebView
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+
+    [self commonInit];
+}
+
+- (void)commonInit {
+    self.scalesPageToFit = YES;
+}
 
 - (instancetype)initWithFrame:(CGRect)frame sessionInfo:(PLRSessionInfo *)sessionInfo merchantKey:(NSString *)merchantKey {
     self = [self initWithFrame:frame];
@@ -36,7 +57,26 @@ static NSString *const APIBaseURL = @"https://sandbox.payler.com/gapi";
     if (!self.sessionInfo) [NSException raise:@"RequiredParameter" format:@"'sessionInfo' is required."];
     if (!self.merchantKey) [NSException raise:@"RequiredParameter" format:@"'merchantKey' is required."];
 
-    
+    self.completionBlock = completion;
+
+    AFHTTPRequestOperationManager *manager = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:[NSURL URLWithString:APIBaseURL]];
+    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] initWithDictionary:@{@"key": self.merchantKey}];
+    [parameters addEntriesFromDictionary:[self.sessionInfo dictionaryRepresentation]];
+
+    [manager POST:@"StartSession" parameters:[parameters copy] success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"#network <%@: %@> \nResponse: %@", operation.request.HTTPMethod, [operation.request.URL absoluteString], responseObject);
+        NSString *sessionId = responseObject[@"session_id"];
+        if (sessionId.length) {
+            NSString *path = [[NSURL URLWithString:@"Pay" relativeToURL:manager.baseURL] absoluteString];
+            NSDictionary *parameters = @{@"session_id": sessionId};
+            NSMutableURLRequest *request = [manager.requestSerializer requestWithMethod:@"POST" URLString:path parameters:parameters error:nil];
+            [self loadRequest:request];
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (completion) {
+            completion(operation.responseObject, error);
+        }
+    }];
 }
 
 #pragma mark - UIWebViewDelegate
@@ -46,7 +86,9 @@ static NSString *const APIBaseURL = @"https://sandbox.payler.com/gapi";
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-
+    if (self.completionBlock) {
+        self.completionBlock(nil, error);
+    }
 }
 
 @end
